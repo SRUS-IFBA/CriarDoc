74cabf373e09f421b8c462159a311b0d24b56b79
abnerscarvalho <abner.s.de.c@gmail.com>
2015-03-22 16:56:36 -0300

    Criação da variável que descreve os pontos de sensibilidade.

--- base/models.py
+++ base/models.py
@@ -4,6 +4,7 @@ class Base(models.Model):
     id= models.AutoField(primary_key=True)
     introducao = models.TextField (blank = false)
     desc_qualidade = models.TextField (blank = false)
+    desc_ponto_sensibilidade = models.TextField (blank = false)
 
 
     def __unicode__(self):
